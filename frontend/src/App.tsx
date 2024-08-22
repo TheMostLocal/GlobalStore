62c95834e64d8095aeeac5ae7b36fc38d9e505c5
TheMostLocal <123339962+TheMostLocal@users.noreply.github.com>
2024-08-22 23:04:37 +0000

    navigation finished?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home/Home" ;
 import Products from "./Products/Products";
