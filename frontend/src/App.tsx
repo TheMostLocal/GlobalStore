ed836f4e16982cab74554e4e13701b9f7f238d80
TheMostLocal <123339962+TheMostLocal@users.noreply.github.com>
2024-08-22 22:10:17 +0000

    new navigation features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home" ;
+import Products from "./Products/Products";
 
 function App() {
   function openMenu(){
@@ -10,11 +10,12 @@ function closeMenu(){
     document.querySelector(".sidebar")?.classList.remove("open");
 }
   return (
-    <div className="grid-container">
-    <head>
+     <Router>
+        <div className="grid-container">
+   
         <link rel="stylesheet" href="style.css"/>
         <title>GoodGlobe Store</title>
-    </head>
+    
     <body>
         <div>
             <header className="header">   
@@ -23,8 +24,8 @@ function closeMenu(){
                     <a href="index.html">GoodGlobe Store</a>
                 </div>
                 <div className="header-links">
-                    <a href="cart.html">Cart</a>
-                    <a href="signin.html">Sign In</a>
+                    <a href="/">Home</a>
+                    <a href="/catalog">Catalog</a>
                 </div>
             </header>
             <aside className="sidebar">
@@ -42,51 +43,19 @@ function closeMenu(){
                     </li>
                 </ul>
             </aside>
-            <main>
-                <div className="content">
-                    <ul className="products">
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li> 
-                        <li>
-                            <div className="product">
-                                <img className="product-image" src="images/d1.jpg" alt="product"/>
-                                <div className="product-name">
-                                    <a href="product.html">Slim Shirt</a>
-                                </div>
-                                <div className="product-brand">Nike</div>
-                                <div className="product-price">$60</div>
-                                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </main>
+            <main className="main">
+                <Routes>
+                    <Route path="/" element= {<Home/>}/>
+                    <Route path="/catalog" element={<Products/>} />
+                </Routes>
+                </main>
             <footer>
                 &copy; 2024 GoodGlobe LLC
             </footer>
         </div>
     </body>
     </div>
+    </Router>
   );
 }
 
