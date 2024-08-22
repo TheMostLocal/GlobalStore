c8810f4ef55671ed4441a2810a05552d2e1b0585
TheMostLocal <123339962+TheMostLocal@users.noreply.github.com>
2024-08-22 22:19:17 +0000

    read imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./Home/Home" ;
 import Products from "./Products/Products";
